0a1a9b8542e6dda9e5bfa2052561feda0f47e33e
taebkhan <taeb.khan@mnscorp.net>
2025-02-22 06:32:19 +0530

    Test 4

--- server/Program.cs
+++ server/Program.cs
@@ -6,7 +6,7 @@ using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add JWT Authentication
+// Add JWT Authentication - 
 var key = Encoding.ASCII.GetBytes(builder.Configuration["Jwt:Secret"]);
 
 builder.Services.AddAuthentication(options =>
